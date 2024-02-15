6ed2589ec64458de0342da4dab807e24f5eefed7
danieldmonteiro <danieldmonteiro@gmail.com>
2024-02-14 22:39:36 -0300

    botao-copiar

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ let textoDescriptografado;
 
 let texto;
 
-let resultadoImagem = document.getElementById("imagem__inicial");
+let caixaResultadoBoxInicio;
+
+let caixaResultadoBoxInserir = document.querySelector("main");
 
 function criptografar() {
-    texto = document.querySelector("textarea").value;
+    texto = document.getElementById("mensagem").value;
+    caixaResultadoBoxInicio = document.getElementById("aplicacao__resultado__id");
     if (texto.length > 4) {
-        if (resultadoImagem == false) {
+        if (caixaResultadoBoxInicio == null) {
             textoCriptografado = texto
                 .toLowerCase()
                 .normalize('NFD').replace(/[\u0300-\u0303\u0327]/g, "")
@@ -19,7 +22,8 @@ function criptografar() {
                 .replace(/u/g, "ufat")
                 .replace(/a/g, "ai")
                 .replace(/o/g, "ober");
-            document.getElementById("texto__inicial").innerHTML = textoCriptografado;
+            let textoResultadoCripto = document.getElementById("mensagem__resultado");
+            textoResultadoCripto.value = textoCriptografado;
         } else {
             textoCriptografado = texto
                 .toLowerCase()
@@ -30,13 +34,22 @@ function criptografar() {
                 .replace(/u/g, "ufat")
                 .replace(/a/g, "ai")
                 .replace(/o/g, "ober");
-            resultadoImagem.remove();
-            document.getElementById("texto__inicial").innerHTML = textoCriptografado;
+            caixaResultadoBoxInicio.remove();
+            caixaResultadoBoxInserir.insertAdjacentHTML("beforeend", '<section class="aplicacao__resultado__box"><div class="caixa__resultado"><textarea id="mensagem__resultado"></textarea></div><div class="aplicacao__resultado__botao" id="botao__copiar"><button onclick="copiar()" class="aplicacao__resultado__botao__copiar">Copiar</button></div></section>');      
+            let textoResultadoCripto = document.getElementById("mensagem__resultado");
+            textoResultadoCripto.value = textoCriptografado;
         };
     } else {
         location.reload();
     };
 };
+
+function copiar() {
+    let caixaTextoDireito = document.getElementById("mensagem__resultado");
+    caixaTextoDireito.select();
+    caixaTextoDireito.setSelectionRange(0, 99999); //copia em dispositivos moveis
+    navigator.clipboard.writeText(caixaTextoDireito.value);
+  };
 
 // function descriptografar() {
 //
